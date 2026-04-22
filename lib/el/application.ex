f7fde2c5ced2d194463e806c970bc053a0b32c95
msuarz <mikemps@gmail.com>
2026-04-21 22:59:29 -0400

    Remove Burrito-specific code from Application

--- lib/el/application.ex
+++ lib/el/application.ex
@@ -4,43 +4,7 @@ defmodule El.Application do
 
   @impl true
   def start(_type, _args) do
-    result = Supervisor.start_link(children(), supervisor_opts())
-    spawn_burrito_if_enabled()
-    result
-  end
-
-  defp spawn_burrito_if_enabled do
-    do_spawn_if_enabled(burrito_enabled?())
-  end
-
-  defp do_spawn_if_enabled(true) do
-    spawn(fn -> run_burrito_cli() end)
-  end
-
-  defp do_spawn_if_enabled(false) do
-    :ok
-  end
-
-  defp burrito_enabled? do
-    is_binary(System.get_env("__BURRITO"))
-  end
-
-  defp run_burrito_cli do
-    safe_execute_burrito_cli()
-  end
-
-  defp safe_execute_burrito_cli do
-    execute_burrito_cli()
-  rescue
-    _ -> :ok
-  catch
-    _kind, _reason -> :ok
-  end
-
-  defp execute_burrito_cli do
-    args = Burrito.Util.Args.argv()
-    El.CLI.main(args)
-    System.halt(0)
+    Supervisor.start_link(children(), supervisor_opts())
   end
 
   def children do
