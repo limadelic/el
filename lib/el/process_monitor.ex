7d22e0e52343f8beb0868c80070fbe574cb810f0
msuarz <mikemps@gmail.com>
2026-04-27 15:46:52 -0400

    Refactor process_monitor.ex functions

--- lib/el/process_monitor.ex
+++ lib/el/process_monitor.ex
@@ -1,13 +1,14 @@
 defmodule El.ProcessMonitor do
   def wait_for_down(ref, name) do
     receive do
-      {:DOWN, ^ref, :process, _, _} ->
-        El.Application.delete_session_messages(name)
-        :ok
+      {:DOWN, ^ref, :process, _, _} -> cleanup(name)
     after
-      5000 ->
-        El.Application.delete_session_messages(name)
-        :ok
+      5000 -> cleanup(name)
     end
+  end
+
+  defp cleanup(name) do
+    El.Application.delete_session_messages(name)
+    :ok
   end
 end
