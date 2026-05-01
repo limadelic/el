02e6df273f1ef75c28008e62dcc4c8aeca79f486
msuarz <mikemps@gmail.com>
2026-04-30 20:59:16 -0400

    Revert "Extend SessionMeta to carry cwd"
    This reverts commit aa480163b989219fda5ae7a5c7c47125c391e61d.

--- lib/el/session_meta.ex
+++ lib/el/session_meta.ex
@@ -1,17 +1,12 @@
 defmodule El.SessionMeta do
   @callback insert(term(), term(), term()) :: term()
-  @callback insert(term(), term(), term(), term()) :: term()
   @callback lookup(term()) :: term()
   @callback delete(term()) :: term()
   @callback close() :: term()
 
   def insert(name, agent, session_id) do
-    insert(name, agent, session_id, nil)
-  end
-
-  def insert(name, agent, session_id, cwd) do
     backend = Application.get_env(:el, :dets_backend, El.DetsBackend)
-    backend.insert(:session_meta, {name, session_id, agent, cwd})
+    backend.insert(:session_meta, {name, session_id, agent})
     :ok
   end
 
@@ -22,7 +17,6 @@ defmodule El.SessionMeta do
     |> match_session()
   end
 
-  defp match_session([{_name, session_id, agent, cwd}]), do: {:ok, session_id, agent, cwd}
   defp match_session([{_name, session_id, agent}]), do: {:ok, session_id, agent}
   defp match_session([]), do: {:error, :not_found}
 
