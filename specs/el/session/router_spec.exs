1b95372b2b7e730f2137689d9c3faed1b0fa24f2
msuarz <mikemps@gmail.com>
2026-05-03 19:24:06 -0400

    Flake-fix: preload Router in router_spec

--- specs/el/session/router_spec.exs
+++ specs/el/session/router_spec.exs
@@ -2,6 +2,11 @@ defmodule El.Session.RouterSpec do
   use ExUnit.Case
   alias El.Session.Router
 
+  setup_all do
+    Code.ensure_loaded!(El.Session.Router)
+    :ok
+  end
+
   test "filter_self_routes drops routes targeting state.name" do
     state = %{name: :test_session}
     routes = [{:test_session, "msg"}, {:other, "msg"}]
