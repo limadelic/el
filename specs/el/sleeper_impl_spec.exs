cd40dd17692c8dffa659e7d6922cb600c92fefa7
msuarz <mikemps@gmail.com>
2026-05-04 18:17:09 -0400

    Replace SleeperImpl real-sleep test with structural behaviour assertion

--- specs/el/sleeper_impl_spec.exs
+++ specs/el/sleeper_impl_spec.exs
@@ -1,14 +1,16 @@
 defmodule El.SleeperImpl.Spec do
   use ExUnit.Case
 
-  describe "El.SleeperImpl.sleep/1" do
-    test "delegates to :timer.sleep and returns :ok" do
-      start = System.monotonic_time(:millisecond)
-      result = El.SleeperImpl.sleep(1)
-      finish = System.monotonic_time(:millisecond)
+  setup_all do
+    Code.ensure_loaded!(El.SleeperImpl)
+    Code.ensure_loaded!(El.Behaviours.Sleeper)
+    :ok
+  end
 
-      assert result == :ok
-      assert finish - start >= 1
+  describe "El.SleeperImpl" do
+    test "declares @behaviour El.Behaviours.Sleeper" do
+      assert El.Behaviours.Sleeper in El.SleeperImpl.module_info(:attributes)[:behaviour] ||
+             []
     end
   end
 end
