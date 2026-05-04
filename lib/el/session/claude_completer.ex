217ec384a87a2797236ca5fd7b7814fc33ff2d07
msuarz <mikemps@gmail.com>
2026-05-04 15:40:05 -0400

    Make AskCompleter target opaque (role-neutral)

--- lib/el/session/claude_completer.ex
+++ lib/el/session/claude_completer.ex
@@ -1,7 +1,7 @@
 defmodule El.Session.ClaudeCompleter do
   @behaviour El.Session.AskCompleter
 
-  def complete(reporter, {from, message, ref, claude_pid}, routes) do
+  def complete(claude_pid, reporter, {from, message, ref}, routes) do
     Task.start(fn ->
       {response, model, session_id} = El.Session.Claude.ask_work(claude_pid, message, routes)
       GenServer.cast(reporter, {:complete_ask, from, message, response, ref, model, session_id})
