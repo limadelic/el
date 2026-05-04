1c99f4ada6bcb8909385e6dc8e338f22c43f1908
msuarz <mikemps@gmail.com>
2026-05-04 17:41:04 -0400

    Split router relay test per one-assertion rule

--- specs/el/session/router_spec.exs
+++ specs/el/session/router_spec.exs
@@ -17,7 +17,7 @@ defmodule El.Session.RouterSpec do
   end
 
   describe "cast_store_relay routing" do
-    test "tell_route_target uses session_api.cast_store_relay for both target and self" do
+    test "tell_route_target relays to target with empty response" do
       test_pid = self()
       expect(El.MockSessionApi, :cast_store_relay, 2, fn name, payload, response ->
         send(test_pid, {:relayed, name, payload, response})
@@ -33,6 +33,23 @@ defmodule El.Session.RouterSpec do
       Router.route_all_tells(state, "msg", [{:target, "payload"}])
 
       assert_receive {:relayed, :target, "[from sender] payload", ""}
+    end
+
+    test "tell_route_target relays to self with target tag" do
+      test_pid = self()
+      expect(El.MockSessionApi, :cast_store_relay, 2, fn name, payload, response ->
+        send(test_pid, {:relayed, name, payload, response})
+        :ok
+      end)
+
+      state = %{
+        name: :sender,
+        session_api: El.MockSessionApi,
+        alive_fn: fn _ -> true end
+      }
+
+      Router.route_all_tells(state, "msg", [{:target, "payload"}])
+
       assert_receive {:relayed, :sender, "msg", "-> target"}
     end
   end
