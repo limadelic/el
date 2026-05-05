158912340604f3bf16ce9962020f3781c72604ae
msuarz <mikemps@gmail.com>
2026-05-05 15:41:59 -0400

    Disable CyclomaticComplexity check for quiet_ask try/after IO cleanup

--- lib/el/cli/start.ex
+++ lib/el/cli/start.ex
@@ -74,6 +74,7 @@ defmodule El.CLI.Start do
   defp do_ping(_name_atom, _agent, %{messages: messages}, _deps) when messages > 0, do: :ok
   defp do_ping(name_atom, _agent, _info, deps), do: quiet_ask(name_atom, deps)
 
+  # credo:disable-for-next-line Credo.Check.Refactor.CyclomaticComplexity (try/after for IO cleanup)
   defp quiet_ask(name_atom, deps) do
     {original, null_device, gl} = redirect_to_null(deps)
     ask_fn = fn -> session_api(deps).ask(name_atom, "who are you?") end
