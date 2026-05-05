e47a28e508fff94f36d0315e2162cd2ebef4498e
msuarz <mikemps@gmail.com>
2026-05-05 14:07:24 -0400

    green quiet_ask ≤5 lines per fn

--- lib/el/cli/start.ex
+++ lib/el/cli/start.ex
@@ -75,16 +75,22 @@ defmodule El.CLI.Start do
   defp do_ping(name_atom, _agent, _info, deps), do: quiet_ask(name_atom, deps)
 
   defp quiet_ask(name_atom, deps) do
+    {original, null_device, gl} = redirect_to_null(deps)
+    ask_fn = fn -> session_api(deps).ask(name_atom, "who are you?") end
+    try(do: ask_fn.(), after: restore_io(gl, original, null_device))
+  end
+
+  defp redirect_to_null(deps) do
     gl = group_leader(deps)
     null_device = gl.open_null_device()
     original = gl.get()
     gl.set(self(), null_device)
-    try do
-      session_api(deps).ask(name_atom, "who are you?")
-    after
-      gl.set(self(), original)
-      gl.close(null_device)
-    end
+    {original, null_device, gl}
+  end
+
+  defp restore_io(gl, original, null_device) do
+    gl.set(self(), original)
+    gl.close(null_device)
   end
 
   def print_session_info(name, opts, deps \\ []) do
