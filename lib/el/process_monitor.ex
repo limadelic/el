12a4fec19188895e083b238cdeba1a29fab07e8b
msuarz <mikemps@gmail.com>
2026-05-04 20:19:07 -0400

    Inject app and timeout into ProcessMonitor.wait_for_down

--- lib/el/process_monitor.ex
+++ lib/el/process_monitor.ex
@@ -1,16 +1,22 @@
 defmodule El.ProcessMonitor do
   @behaviour El.Behaviours.Monitor
 
-  def wait_for_down(ref, name) do
+  def wait_for_down(ref, name, opts \\ []) do
+    timeout = Keyword.get(opts, :timeout, 5000)
+    app = Keyword.get(opts, :app, El.Application)
+    do_wait(ref, name, timeout, app)
+  end
+
+  defp do_wait(ref, name, timeout, app) do
     receive do
-      {:DOWN, ^ref, :process, _, _} -> cleanup(name)
+      {:DOWN, ^ref, :process, _, _} -> cleanup(name, app)
     after
-      5000 -> cleanup(name)
+      timeout -> cleanup(name, app)
     end
   end
 
-  defp cleanup(name) do
-    El.Application.delete_session_messages(name)
+  defp cleanup(name, app) do
+    app.delete_session_messages(name)
     :ok
   end
 end
---
+++ specs/el/process_monitor_spec.exs
@@ -0,0 +1,41 @@
+defmodule El.ProcessMonitorSpec do
+  use ExUnit.Case
+
+  import Mox
+
+  setup :verify_on_exit!
+
+  describe "wait_for_down/3" do
+    test "calls app.delete_session_messages(name) when :DOWN arrives" do
+      expect(El.MockApp, :delete_session_messages, fn :test_session -> :ok end)
+
+      ref = Process.monitor(self())
+      send(self(), {:DOWN, ref, :process, self(), :normal})
+
+      result = El.ProcessMonitor.wait_for_down(ref, :test_session, app: El.MockApp)
+
+      assert result == :ok
+    end
+
+    test "calls app.delete_session_messages(name) after timeout" do
+      expect(El.MockApp, :delete_session_messages, fn :test_session -> :ok end)
+
+      ref = Process.monitor(self())
+
+      result = El.ProcessMonitor.wait_for_down(ref, :test_session, app: El.MockApp, timeout: 0)
+
+      assert result == :ok
+    end
+
+    test "returns :ok" do
+      expect(El.MockApp, :delete_session_messages, fn :test_session -> :ok end)
+
+      ref = Process.monitor(self())
+      send(self(), {:DOWN, ref, :process, self(), :normal})
+
+      result = El.ProcessMonitor.wait_for_down(ref, :test_session, app: El.MockApp)
+
+      assert result == :ok
+    end
+  end
+end
