66aae98caa70a655fa749c05a70dda2ca3d6879c
msuarz <mikemps@gmail.com>
2026-05-05 22:19:39 -0400

    Apply Mox import and one-fact rule to spec

--- specs/el/claude_port/connection/cli_resolver_spec.exs
+++ specs/el/claude_port/connection/cli_resolver_spec.exs
@@ -1,10 +1,15 @@
 defmodule El.ClaudePort.Connection.CliResolverSpec do
   use ExUnit.Case
+  import Mox
+  setup :verify_on_exit!
 
-  test "resolve/3 returns ok tuple with executable and args" do
+  test "resolve/3 returns ok with binary executable" do
     result = El.ClaudePort.Connection.CliResolver.resolve("/path/to/cli", [], "resume-123")
-    assert {:ok, {executable, args}} = result
-    assert is_binary(executable)
-    assert is_list(args)
+    assert match?({:ok, {executable, _args}} when is_binary(executable), result)
+  end
+
+  test "resolve/3 returns ok with list of args" do
+    result = El.ClaudePort.Connection.CliResolver.resolve("/path/to/cli", [], "resume-123")
+    assert match?({:ok, {_executable, args}} when is_list(args), result)
   end
 end
