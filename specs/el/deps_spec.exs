2c82b9c00adba52b4e0ae94a3c4fe987e50446a8
msuarz <mikemps@gmail.com>
2026-05-05 21:13:59 -0400

    Add restorer_module seam in deps

--- specs/el/deps_spec.exs
+++ specs/el/deps_spec.exs
@@ -7,5 +7,11 @@ defmodule El.Deps.Spec do
 
       assert Keyword.get(result, :init_module) == El.MessageStore.Init
     end
+
+    test "returns map with restorer_module key defaulting to El.SessionRestorer" do
+      result = El.Deps.production()
+
+      assert Keyword.get(result, :restorer_module) == El.SessionRestorer
+    end
   end
 end
