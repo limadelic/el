8f8d47e21804a08b1f94bcafa225fb39a501e4cb
msuarz <mikemps@gmail.com>
2026-05-06 15:21:15 -0400

    Rename El.SessionAdapter to El.Adapters.Session

--- lib/el/adapters/session.ex
+++ lib/el/adapters/session.ex
@@ -1,4 +1,4 @@
-defmodule El.SessionAdapter do
+defmodule El.Adapters.Session do
   @callback start_link(opts :: keyword()) :: {:ok, pid()} | {:error, term()}
   @callback stream(pid :: pid(), prompt :: String.t()) :: term()
 end
