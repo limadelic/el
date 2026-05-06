0c2e499a90a8a4334e6e422e0b9699cbe2e90d00
msuarz <mikemps@gmail.com>
2026-05-06 16:39:43 -0400

    Extract Store behaviour to MessageStore.Behaviours

--- lib/el/behaviours.ex
+++ lib/el/behaviours.ex
@@ -59,20 +59,6 @@ defmodule El.Behaviours.FileSystem do
   @callback mkdir_p!(String.t()) :: :ok
 end
 
-defmodule El.Behaviours.Store do
-  @callback delete_ask_entry(term(), term(), term()) :: term()
-  @callback store_ask_entry(term(), term()) :: term()
-  @callback replace_ask(term(), term(), term(), term(), term()) :: term()
-  @callback delete_message(term(), term()) :: term()
-  @callback delete_message(term(), term(), keyword()) :: term()
-  @callback store_message(term(), term()) :: term()
-  @callback store_message(term(), term(), keyword()) :: term()
-  @callback delete_session_messages(term()) :: term()
-  @callback delete_session_messages(term(), keyword()) :: term()
-  @callback load_messages(term()) :: term()
-  @callback load_messages(term(), keyword()) :: term()
-end
-
 defmodule El.Behaviours.Sleeper do
   @callback sleep(integer()) :: :ok
 end
--- specs/test_helper.exs
+++ specs/test_helper.exs
@@ -97,7 +97,7 @@ end
 Mox.defmock(El.MockSessionApi, for: El.Behaviours.Session)
 Mox.defmock(El.MockClaudeCode, for: El.Platform.Behaviours.Code)
 Mox.defmock(El.MockClaudeCodeSession, for: El.Platform.Behaviours.CodeSession)
-Mox.defmock(El.MockStoreModule, for: El.Behaviours.Store)
+Mox.defmock(El.MockStoreModule, for: El.MessageStore.Behaviours.Store)
 Mox.defmock(El.MockSessionMeta, for: El.SessionMeta)
 Mox.defmock(El.MockSessionAsk, for: El.Behaviours.SessionAsk)
 Mox.defmock(El.MockDets, for: El.MessageStore.Behaviours.Dets)
