bee9b86502552d46a1c484ae97e4b772e8c7ee57
msuarz <mikemps@gmail.com>
2026-05-05 21:00:09 -0400

    Trim deps_spec to one assertion

--- specs/el/deps_spec.exs
+++ specs/el/deps_spec.exs
@@ -5,8 +5,6 @@ defmodule El.Deps.Spec do
     test "returns map with init_module key defaulting to El.MessageStore.Init" do
       result = El.Deps.production()
 
-      assert is_list(result)
-      assert Keyword.has_key?(result, :init_module)
       assert Keyword.get(result, :init_module) == El.MessageStore.Init
     end
   end
