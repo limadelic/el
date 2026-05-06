c835960cb798bc3510252134ea7effe18a16eb79
msuarz <mikemps@gmail.com>
2026-05-05 21:56:45 -0400

    Split Session.State spec to one fact per test

--- specs/el/session/state_spec.exs
+++ specs/el/session/state_spec.exs
@@ -1,9 +1,13 @@
 defmodule El.Session.StateSpec do
   use ExUnit.Case
 
-  test "build/5 returns a map with :name key" do
+  test "build/5 returns a map" do
     result = El.Session.State.build(:test, [], [], "session-123", "/tmp")
     assert is_map(result)
+  end
+
+  test "build/5 puts name in result" do
+    result = El.Session.State.build(:test, [], [], "session-123", "/tmp")
     assert result[:name] == :test
   end
 end
