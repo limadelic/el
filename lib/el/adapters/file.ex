9f71c53ec468b1668180f27cb44a9cfb3c00ba1f
msuarz <mikemps@gmail.com>
2026-05-06 15:32:24 -0400

    Remove orphan El.Adapters modules

--- lib/el/adapters/file.ex
+++
@@ -1,8 +0,0 @@
-defmodule El.Adapters.File do
-  @callback open(path :: charlist(), options :: list()) ::
-              {:ok, term()} | {:error, term()}
-  @callback write(io_device :: term(), data :: binary()) ::
-              :ok | {:error, term()}
-  @callback read(io_device :: term(), length :: pos_integer()) ::
-              {:ok, binary()} | {:error, term()}
-end
