6e5e4268486e00ada2b56d884957cb351ed44490
msuarz <mikemps@gmail.com>
2026-05-05 22:22:12 -0400

    Lift CliResolver SUT call into setup

--- specs/el/claude_port/connection/cli_resolver_spec.exs
+++ specs/el/claude_port/connection/cli_resolver_spec.exs
@@ -3,13 +3,15 @@ defmodule El.ClaudePort.Connection.CliResolverSpec do
   import Mox
   setup :verify_on_exit!
 
-  test "resolve/3 returns ok with binary executable" do
-    result = El.ClaudePort.Connection.CliResolver.resolve("/path/to/cli", [], "resume-123")
+  setup do
+    {:ok, result: El.ClaudePort.Connection.CliResolver.resolve("/path/to/cli", [], "resume-123")}
+  end
+
+  test "resolve/3 returns ok with binary executable", %{result: result} do
     assert match?({:ok, {executable, _args}} when is_binary(executable), result)
   end
 
-  test "resolve/3 returns ok with list of args" do
-    result = El.ClaudePort.Connection.CliResolver.resolve("/path/to/cli", [], "resume-123")
+  test "resolve/3 returns ok with list of args", %{result: result} do
     assert match?({:ok, {_executable, args}} when is_list(args), result)
   end
 end
