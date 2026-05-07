846e555302212ff12beee54e548f2721426c2ed3
msuarz <mikemps@gmail.com>
2026-05-07 13:04:45 -0400

    Extract El.CLI.Daemon.Env, reduce daemon.ex module length

--- lib/el/cli/daemon.ex
+++ lib/el/cli/daemon.ex
@@ -1,13 +1,10 @@
 defmodule El.CLI.Daemon do
   @behaviour El.CLI.Behaviours.Daemon
 
-  def daemon_script do
-    :escript.script_name() |> to_string() |> Path.expand()
-  end
-
-  def daemon_node do
-    dev?() |> daemon_node_for()
-  end
+  defdelegate daemon_script, to: El.CLI.Daemon.Env
+  defdelegate daemon_node, to: El.CLI.Daemon.Env
+  defdelegate daemon_cookie, to: El.CLI.Daemon.Env
+  defdelegate dev?, to: El.CLI.Daemon.Env
 
   def stop_daemon(opts \\ [])
   def stop_daemon(opts) when is_list(opts) do
@@ -64,33 +61,9 @@ defmodule El.CLI.Daemon do
     node_connector.set_cookie(daemon_cookie())
   end
 
-  def dev? do
-    dev_check(System.get_env("DEV"))
-  end
-
   def ensure_daemon(system \\ El.Infra.System, node_connector \\ El.Infra.NodeConnector) do
     ensure_daemon_connected(node_connector.connect(daemon_node()), system, node_connector)
   end
-
-  defp daemon_node_for(true), do: :"el_dev@127.0.0.1"
-  defp daemon_node_for(false), do: :"el@127.0.0.1"
-
-  defp daemon_cookie_for(true), do: :el_dev
-  defp daemon_cookie_for(false), do: :el
-
-  defp daemon_cookie do
-    dev?() |> daemon_cookie_for()
-  end
-
-  defp dev_check(nil), do: script_is_relative()
-  defp dev_check(_), do: true
-
-  defp script_is_relative do
-    :escript.script_name() |> to_string() |> Path.type() |> is_relative()
-  end
-
-  defp is_relative(:relative), do: true
-  defp is_relative(_), do: false
 
   defp start_client_node(net_kernel, node_connector) do
     id = System.unique_integer([:positive])
