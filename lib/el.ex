68c753e3184b5fcc00d06f53dcc964d2bc1a814f
msuarz <mikemps@gmail.com>
2026-05-06 23:20:58 -0400

    TCR: auto-commit

--- lib/el.ex
+++ lib/el.ex
@@ -96,7 +96,7 @@ defmodule El do
 
   @impl true
   def ls(opts \\ []) do
-    session_registry = Keyword.get(opts, :session_registry, El.Session.Registry)
+    session_registry = Application.get_env(:el, :session_registry, El.Session.Registry)
     session_registry.list(opts)
   end
 end
