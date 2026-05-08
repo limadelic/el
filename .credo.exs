055dbb126cf1bb69474d9a800e85a9a8e2017e08
msuarz <mikemps@gmail.com>
2026-05-08 15:34:31 -0400

    Revert "add FolderSize credo check skeleton"
    This reverts commit 045ea8792e63288f9f8d134853f124b3650bd20c.

--- .credo.exs
+++ .credo.exs
@@ -76,7 +76,6 @@
           {Credo.Check.Design.DuplicatedCode, mass_threshold: 40},
           {El.Credo.MaxFunctionLines, [max_lines: 5]},
           {El.Credo.MaxModuleLines, [max_lines: 100]},
-          {El.Credo.FolderSize, []},
           {Credo.Check.Warning.Dbg, []},
           {Credo.Check.Warning.IoInspect, []},
           {Credo.Check.Warning.IExPry, []},
