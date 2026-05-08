a713fd3dec4ad000125da677a42bdfe62411f983
msuarz <mikemps@gmail.com>
2026-05-08 15:15:47 -0400

    delete orphan format_uuid

--- lib/el/session/id.ex
+++ lib/el/session/id.ex
@@ -1,6 +1,2 @@
 defmodule El.Session.Id do
-  def format_uuid(hex) do
-    <<a::binary-8, b::binary-4, c::binary-4, d::binary-4, e::binary-12>> = hex
-    Enum.join([a, b, c, d, e], "-")
-  end
 end
