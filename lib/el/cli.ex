28c2c256550282450e3d04e0b9f7ed0064d32524
msuarz <mikemps@gmail.com>
2026-05-08 15:20:53 -0400

    extract maybe_put helper from print_info

--- lib/el/cli.ex
+++ lib/el/cli.ex
@@ -101,11 +101,14 @@ defmodule El.CLI do
     info = session_api.info(name_atom)
     model = info[:model]
     opts = []
-    opts = if agent, do: [agent: agent] ++ opts, else: opts
-    opts = if model, do: [model: model] ++ opts, else: opts
+    opts = opts |> maybe_put(:agent, agent) |> maybe_put(:model, model)
     Start.print_session_info(name, opts, deps)
   end
   defp print_info(false, _name, _session_api, _deps), do: IO.puts(Output.usage_message())
+
+  defp maybe_put(opts, _key, nil), do: opts
+  defp maybe_put(opts, key, value), do: [{key, value} | opts]
+
   defp maybe_print_card(:created, name, opts, deps), do: Start.print_session_info(name, opts, deps)
   defp maybe_print_card(:already_running, _name, _opts, _deps), do: :ok
 end
