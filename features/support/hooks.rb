e72987a0159fb32535e71848f4620c99af33ecfd
msuarz <mikemps@gmail.com>
2026-05-08 09:26:19 -0400

    Revert "el tag interpolation: append start subcommand"
    This reverts commit 87e392ecff0188a27b4c7ca5fb48503a81c20d25.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -7,7 +7,7 @@ rescue Timeout::Error
 end
 
 Before do |scenario|
-  scenario.tags.map(&:name).grep(/^el_(.+)$/) { el("#{$1} start") }
+  scenario.tags.map(&:name).grep(/^el_(.+)$/) { el($1) }
 end
 
 After do |scenario|
